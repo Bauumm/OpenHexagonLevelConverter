6acfe3bbcfd8af3c13a958e964b4e9fa2bf34b47
Bauumm <oh4@web.de>
2022-09-20 21:31:54 +0200

    forgot add

---
+++ rotation.lua
@@ -0,0 +1,8 @@
+function prefix_update_rotation(frametime)
+	local next_rotation = math.abs(getLevelValueFloat("rotation_speed") * 10 * frametime)
+	if prefix_fast_spin > 0 then
+		next_rotation = next_rotation + math.abs((prefix_get_smoother_step(0, l_getFastSpin(), prefix_fast_spin) / 3.5) * frametime * 17)
+		prefix_fast_spin = prefix_fast_spin - frametime
+	end
+	l_setRotation(l_getRotation() + next_rotation * prefix_sign(getLevelValueFloat("rotation_speed")))
+end
