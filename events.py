837dcbc21a2cbba35afb83f9e71271d87a96f31d
Bauumm <oh4@web.de>
2022-09-25 19:07:39 +0200

    fix path capitalization in script_exec event

--- events.py
+++ events.py
@@ -1,6 +1,7 @@
 from extended_dict import ExtendedDict
 from config import CONVERTER_PREFIX
 from lua_file import LuaFile
+import fix_utils
 import log
 import os
 
@@ -15,42 +16,42 @@ EVENT_TYPES = {
     "time_stop": CONVERTER_PREFIX + "time_stop = <duration>",
     "timeline_wait": "wait(<duration>)",
     "timeline_clear": CONVERTER_PREFIX + "clear_and_reset_timeline()",
-    "level_float_set": "setLevelValueFloat(\"<valueName>\", <value>)",
-    "level_float_add": "setLevelValueFloat(\"<valueName>\", \
-getLevelValueFloat(\"<valueName>\") + <value>)",
-    "level_float_subtract": "setLevelValueFloat(\"<valueName>\", \
-getLevelValueFloat(\"<valueName>\") - <value>)",
-    "level_float_multiply": "setLevelValueFloat(\"<valueName>\", \
-getLevelValueFloat(\"<valueName>\") * <value>)",
-    "level_float_divide": "setLevelValueFloat(\"<valueName>\", \
-getLevelValueFloat(\"<valueName>\") / <value>)",
-    "level_int_set": "setLevelValueInt(\"<valueName>\", <value>)",
-    "level_int_add": "setLevelValueInt(\"<valueName>\", \
-getLevelValueFloat(\"<valueName>\") + <value>)",
-    "level_int_subtract": "setLevelValueInt(\"<valueName>\", \
-getLevelValueFloat(\"<valueName>\") - <value>)",
-    "level_int_multiply": "setLevelValueInt(\"<valueName>\", \
-getLevelValueFloat(\"<valueName>\") * <value>)",
-    "level_int_divide": "setLevelValueInt(\"<valueName>\", \
-getLevelValueFloat(\"<valueName>\") / <value>)",
-    "style_float_set": "setStyleValueFloat(\"<valueName>\", <value>)",
-    "style_float_add": "setStyleValueFloat(\"<valueName>\", \
-getLevelValueFloat(\"<valueName>\") + <value>)",
-    "style_float_subtract": "setStyleValueFloat(\"<valueName>\", \
-getLevelValueFloat(\"<valueName>\") - <value>)",
-    "style_float_multiply": "setStyleValueFloat(\"<valueName>\", \
-getLevelValueFloat(\"<valueName>\") * <value>)",
-    "style_float_divide": "setStyleValueFloat(\"<valueName>\", \
-getLevelValueFloat(\"<valueName>\") / <value>)",
-    "style_int_set": "setStyleValueInt(\"<valueName>\", <value>)",
-    "style_int_add": "setStyleValueInt(\"<valueName>\", \
-getLevelValueFloat(\"<valueName>\") + <value>)",
-    "style_int_subtract": "setStyleValueInt(\"<valueName>\", \
-getLevelValueFloat(\"<valueName>\") - <value>)",
-    "style_int_multiply": "setStyleValueInt(\"<valueName>\", \
-getLevelValueFloat(\"<valueName>\") * <value>)",
-    "style_int_divide": "setStyleValueInt(\"<valueName>\", \
-getLevelValueFloat(\"<valueName>\") / <value>)",
+    "level_float_set": "setLevelValueFloat(\"<value_name>\", <value>)",
+    "level_float_add": "setLevelValueFloat(\"<value_name>\", \
+getLevelValueFloat(\"<value_name>\") + <value>)",
+    "level_float_subtract": "setLevelValueFloat(\"<value_name>\", \
+getLevelValueFloat(\"<value_name>\") - <value>)",
+    "level_float_multiply": "setLevelValueFloat(\"<value_name>\", \
+getLevelValueFloat(\"<value_name>\") * <value>)",
+    "level_float_divide": "setLevelValueFloat(\"<value_name>\", \
+getLevelValueFloat(\"<value_name>\") / <value>)",
+    "level_int_set": "setLevelValueInt(\"<value_name>\", <value>)",
+    "level_int_add": "setLevelValueInt(\"<value_name>\", \
+getLevelValueFloat(\"<value_name>\") + <value>)",
+    "level_int_subtract": "setLevelValueInt(\"<value_name>\", \
+getLevelValueFloat(\"<value_name>\") - <value>)",
+    "level_int_multiply": "setLevelValueInt(\"<value_name>\", \
+getLevelValueFloat(\"<value_name>\") * <value>)",
+    "level_int_divide": "setLevelValueInt(\"<value_name>\", \
+getLevelValueFloat(\"<value_name>\") / <value>)",
+    "style_float_set": "setStyleValueFloat(\"<value_name>\", <value>)",
+    "style_float_add": "setStyleValueFloat(\"<value_name>\", \
+getLevelValueFloat(\"<value_name>\") + <value>)",
+    "style_float_subtract": "setStyleValueFloat(\"<value_name>\", \
+getLevelValueFloat(\"<value_name>\") - <value>)",
+    "style_float_multiply": "setStyleValueFloat(\"<value_name>\", \
+getLevelValueFloat(\"<value_name>\") * <value>)",
+    "style_float_divide": "setStyleValueFloat(\"<value_name>\", \
+getLevelValueFloat(\"<value_name>\") / <value>)",
+    "style_int_set": "setStyleValueInt(\"<value_name>\", <value>)",
+    "style_int_add": "setStyleValueInt(\"<value_name>\", \
+getLevelValueFloat(\"<value_name>\") + <value>)",
+    "style_int_subtract": "setStyleValueInt(\"<value_name>\", \
+getLevelValueFloat(\"<value_name>\") - <value>)",
+    "style_int_multiply": "setStyleValueInt(\"<value_name>\", \
+getLevelValueFloat(\"<value_name>\") * <value>)",
+    "style_int_divide": "setStyleValueInt(\"<value_name>\", \
+getLevelValueFloat(\"<value_name>\") / <value>)",
     "music_set": "a_setMusic(\"<id>\")",
     "music_set_segment": "a_setMusicSegment(\"<id>\", \"<segment_index>\")",
     "music_set_seconds": "a_setMusicSeconds(\"<id>\", \"<seconds>\")",
@@ -61,7 +62,7 @@ getLevelValueFloat(\"<valueName>\") / <value>)",
     "increment_start": CONVERTER_PREFIX + "increment_enabled = true",
     "event_exec": "execEvent(\"<id>\")",
     "event_enqueue": "enqueueEvent(\"<id>\")",
-    "script_exec": "u_execScript(\"<valueName>\")",
+    "script_exec": "u_execScript(\"<value_name>\")",
     "play_sound": "playSound(\"<id>\")"
 }
 
@@ -69,12 +70,12 @@ event_lua = LuaFile(os.path.join(os.path.dirname(__file__), "events.lua"))
 event_lua.replace("prefix_", CONVERTER_PREFIX)
 
 
-def convert_event(event_json):
+def convert_event(event_json, pack_path):
     # set default values
     event = {
         "type": event_json.get("type"),
         "duration": event_json.get("duration", 0),
-        "valueName": event_json.get("value_name", ""),
+        "value_name": event_json.get("value_name", ""),
         "value": event_json.get("value", 0),
         "message": event_json.get("message", ""),
         "id": event_json.get("id", ""),
@@ -92,6 +93,11 @@ def convert_event(event_json):
             return ""
         if event["type"] == "level_change":
             event["id"] = os.path.basename(event["id"])
+        elif event["type"] == "script_exec":
+            real_path = fix_utils.match_capitalization(
+                os.path.join(pack_path, "Scripts", event["value_name"]))
+            event["value_name"] = os.path.relpath(real_path, os.path.join(
+                pack_path, "Scripts"))
         for prop in event:
             function = function.replace("<" + prop + ">", str(event[prop]))
         return function.replace("\n", "\\n").replace("\\", "\\\\")
@@ -99,13 +105,13 @@ def convert_event(event_json):
         return "u_log(\"unkown event type: " + event["type"] + "\")"
 
 
-def convert_events(events):
+def convert_events(events, pack_path):
     if events is None:
         events = []
     event_dict = ExtendedDict()
     for event in events:
         if event is not None:  # this can happen with faulty json
-            event_function = convert_event(event)
+            event_function = convert_event(event, pack_path)
             if event_function is not None:
                 event_list = event_dict.get(event.get("time", 0), [])
                 event_list.append(event_function)
@@ -114,11 +120,14 @@ def convert_events(events):
 
 
 def convert_external(json_file):
+    pack_path = os.path.dirname(json_file.path)
+    while not os.path.exists(os.path.join(pack_path, "pack.json")):
+        pack_path = os.path.dirname(pack_path)
     if "id" in json_file:
         new_event_lua = LuaFile()
         new_event_lua.set_text("_G[\"" + CONVERTER_PREFIX + json_file["id"] +
                                "_EVENTS\"]=" + convert_events(
-                                   json_file.get("events", []))
+                                   json_file.get("events", []), pack_path)
                                .to_table() + "\n")
         new_event_lua.save("Scripts/" + CONVERTER_PREFIX + "Events/" +
                            json_file["id"] + ".lua")
@@ -127,10 +136,14 @@ def convert_external(json_file):
 
 
 def convert_level(level_json, level_lua):
+    pack_path = os.path.dirname(level_json.path)
+    while not os.path.exists(os.path.join(pack_path, "pack.json")):
+        pack_path = os.path.dirname(pack_path)
     if level_lua.get_function("onInit") is None:
         level_lua.mixin_line("function onInit()\nend")
     level_lua.mixin_line(CONVERTER_PREFIX + "MAIN_EVENTS=" +
-                         convert_events(level_json.get("events", []))
+                         convert_events(level_json.get("events", []),
+                                        pack_path)
                          .to_table(), "onInit")
     level_json.delete("events")
 
